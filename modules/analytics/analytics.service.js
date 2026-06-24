4bc7da66c1de8b4b577f97d0c168e16fb19766c1
TeoPapadopoulos <ppdpls.theo@gmail.com>
2026-06-24 22:07:51 +0300

    fix: normalize metric string response

--- modules/analytics/analytics.service.js
+++ modules/analytics/analytics.service.js
@@ -26,9 +26,41 @@ export function createAnalyticsService(analyticsRepos) {
     async getLeaderboard(guildId, range, metric, limit) {
       const { from, to } = range;
       const data = await analyticsRepos.leaderboards.getLeaderboard(
-        guildId, from, to, limit, metric,
+        guildId,
+        from,
+        to,
+        limit,
+        metric,
       );
-      return { metric, limit, ...data };
-    }
+      let normalizedMetric = '';
+      switch (metric) {
+        case 'activeTime':
+          normalizedMetric = 'Active Time';
+          break;
+        case 'cameraTime':
+          normalizedMetric = 'Camera Time';
+          break;
+        case 'streamingTime':
+          normalizedMetric = 'Streaming Time';
+          break;
+        case 'soloTime':
+          normalizedMetric = 'Solo Time';
+          break;
+        case 'deafenedTime':
+          normalizedMetric = 'Deafened Time';
+          break;
+        case 'mutedTime':
+          normalizedMetric = 'Muted Time';
+          break;
+        case 'activeDays':
+          normalizedMetric = 'Active Days';
+          break;
+        default:
+          normalizedMetric = 'Active Time';
+          break;
+      }
+
+      return { metric: normalizedMetric, limit, ...data };
+    },
   };
 }
